5a5f854b0617e605bce4f673e08301098fb346c6
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-02-02 01:45:13 -0300

    feat: classe ContaTerminal adicionada ao projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Project.");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        Conta conta = new Conta();
+
+        System.out.print("Por favor, digite o número da agência:");
+        conta.setAgencia(scanner.nextLine());
+
+        System.out.print("Por favor, digite o número da conta:");
+        conta.setNumero(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do cliente:");
+        conta.setNomeCliente(scanner.nextLine());
+
+        System.out.print("Por favor, digite o saldo inicial:");
+        while(!scanner.hasNextDouble()) {
+            System.out.print("Valor inválido. Por favor, digite um número para o saldo inicial:");
+            scanner.next();
+        }
+        double valor = scanner.nextDouble();
+        conta.depositar(valor);
+
+        scanner.close();
+
+        System.out.println("\n ============ DADOS DA CONTA ============ ");
+        System.out.println(conta.toString());
     }
 }
